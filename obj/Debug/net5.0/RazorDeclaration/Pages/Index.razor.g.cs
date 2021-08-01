d09b13f92d3d3b29fa9eb48870379ac3a15e7f90
Global Admin <globaladmin@azureadmindev.onmicrosoft.com>
2021-08-01 20:30:34 +0100

    Amend homepage

--- obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,77 @@ namespace ticktocksprintclock.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 1 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 2 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 3 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 4 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 5 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 6 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 7 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 8 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 9 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using ticktocksprintclock;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
+#line 10 "c:\Users\wjones\source\repos\ticktocksprintclock\_Imports.razor"
 using ticktocksprintclock.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\wjones\source\repos\ticktocksprintclock\Pages\Index.razor"
+#line 2 "c:\Users\wjones\source\repos\ticktocksprintclock\Pages\Index.razor"
 using Microsoft.AspNetCore.WebUtilities;
 
 #line default
@@ -98,7 +98,7 @@ using Microsoft.AspNetCore.WebUtilities;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 133 "C:\Users\wjones\source\repos\ticktocksprintclock\Pages\Index.razor"
+#line 149 "c:\Users\wjones\source\repos\ticktocksprintclock\Pages\Index.razor"
        
 
     private string _projectName = "My Project";
@@ -114,6 +114,8 @@ using Microsoft.AspNetCore.WebUtilities;
 
     private int _workingDaysInWeek = 5;
 
+    private string _link;
+
     private DateTime _projectStartDate;
     private DateTime _projectEndDate;
     private DateTime _sprintStartDate;
@@ -124,6 +126,8 @@ using Microsoft.AspNetCore.WebUtilities;
 
     private bool showEdit;
     private bool showAdvanced;
+
+    private bool showLink;
 
 
     protected override void OnInitialized()
@@ -178,13 +182,20 @@ using Microsoft.AspNetCore.WebUtilities;
         _projectEndDate = _projectStartDate.AddDays(_sprintLength * _numberOfSprints);
         _daysLeftInProject = (_projectEndDate - DateTime.Now).Days;
         _workingDayLeftInProject = ((_projectEndDate - DateTime.Now).Days / 7) * _workingDaysInWeek;
-        _workingHoursLeftInSprint = ((((_sprintEndDate - DateTime.Now).TotalHours) / 24 ) * _workingHoursInDay);
+        _workingHoursLeftInSprint = ((((_sprintEndDate - DateTime.Now).TotalHours) / 24) * _workingHoursInDay);
     }
 
     private void btnEdit()
     {
         showEdit = true;
         StateHasChanged();
+    }
+
+    private void btnGenerateLink()
+    {
+        showLink = true;
+        StateHasChanged();
+
     }
 
     private void btnRecalculate()
